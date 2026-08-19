25c6546ef6da8d4234c65c7e4acb79540b51ae6d
Robson Machado Silva <robsnmchado@gmail.com>
2026-08-19 01:12:27 +0000

    Aula 1808]

--- aula00/curso_aluno.sql
+++ aula00/curso_aluno.sql
@@ -1,4 +1,4 @@
--- Active: 1787005276179@@127.0.0.1@5432@bd_aula@public
+-- Active: 1787096545685@@127.0.0.1@5432@bd_aula@public
 CREATE TABLE curso(
     id_curso INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nome VARCHAR(60) NOT NULL UNIQUE
@@ -27,3 +27,41 @@ INSERT INTO aluno (nome, id_curso) VALUES
 ('Fernando Rocha', 1)
 
 SELECT id_aluno, nome, id_curso FROM aluno ORDER BY id_aluno ASC;
+
+SELECT table_name,
+       column_name,
+       data_type,
+       character_maximum_length AS tamanho,
+       is_nullable              AS aceita_nulo,
+       is_identity              AS e_identidade
+FROM information_schema.columns
+WHERE table_schema = 'public'
+  AND table_name IN ('curso', 'aluno')
+ORDER BY table_name, ordinal_position;
+
+SELECT
+    nome,
+    id_curso
+FROM
+    aluno
+WHERE
+    id_curso = 1
+ORDER BY
+    nome DESC;
+
+SELECT
+    a.nome AS aluno,
+    c.nome AS curso
+FROM
+    aluno a
+    JOIN curso c ON c.id_curso = a.id_curso
+ORDER BY c.nome;
+
+SELECT 
+    c.nome AS curso,
+    COUNT(a.id_aluno) AS qnt_alunos
+FROM
+    curso c
+    JOIN aluno a ON a.id_curso = c.id_curso
+GROUP BY curso
+ORDER BY qnt_alunos DESC;
